e17fbdcc117fcc70acd827b0bea640c525450288
Asher Lachoff <bladewielder@ashers-imac.attlocal.net>
2021-04-20 00:45:12 -0700

    cosmic backpedal

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,23 +4,11 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-
-        this.starfield = this.add.tileSprite(
-            0,0,640,480, 'starfield',
-        ).setOrigin(0,0);
-
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00,).setOrigin(0,0);
-        
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-        
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.text(20, 20, "Rocket Patrol Play")
     }
-
+/*
     update() {
         this.starfield.tilePositionX -= 4;
     }
+    */
 }

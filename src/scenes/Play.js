d173d46b6a0cd16e5001741b31c55a7f86231816
Asher Lachoff <bladewielder@ashers-imac.attlocal.net>
2021-06-24 04:00:45 -0700

    added explosion animations

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -57,15 +57,15 @@ class Play extends Phaser.Scene {
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)) {
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)) {
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
@@ -79,5 +79,18 @@ class Play extends Phaser.Scene {
             } else {
                 return false;
             }
+    }
+
+    shipExplode(ship) {
+        // temporarily hide the ship
+        ship.alpha = 0;
+        // create explosion sprite at ship's pos
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');
+        boom.on('animationcomplete', () => {
+            ship.reset();
+            ship.alpha = 1;
+            boom.destroy();
+        });
     }
 }

f367dbe604d890171b6a09bc26c2538620aa0014
Asher Lachoff <bladewielder@ashers-imac.attlocal.net>
2021-07-04 18:08:53 -0700

    added point breakdown to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+// POINT BREAKDOWN: 
+// Starting tier: 30 pts ( everything except the last one )
+// Novice tier: 0 pts
+// Intermediate tier: 0 pts
+// S(hrek) tier: 0 pts
+// #FACADE tier: 0 pts
 let config = {
     type: Phaser.CANVAS,
     width: 640,
